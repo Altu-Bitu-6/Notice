65cdd4320d9e0abbd7b24576ab2d4918cb78aff3
Hanby Choi <76518934+hanby-choi@users.noreply.github.com>
2024-03-16 11:39:42 +0900

    [브루트포스] 샘플 코드 업데이트 - 11723

--- 04_브루트포스/필수/11723.cpp
+++ 04_브루트포스/필수/11723.cpp
@@ -1,8 +1,6 @@
 // 11723: 집합 (실버5/비트마스킹)
 #include <iostream>
-
 using namespace std;
-
 /**[백준 11723] 집합
 /* - vector나 map으로 각 원소의 유무를 판단할 경우 시간초과 발생!
 /*   => 총 원소의 개수는 20. 32비트 크기 int의 각 bit에 원소의 유무를 저장
@@ -10,43 +8,68 @@ using namespace std;
 /* - value의 i번째 bit가 1이면: 현재 집합에 i 존재 X
  */
 
+void all(int& s){
+    s = (1 << 21) - 1;
+}
+
+void empty(int& s){
+    s = 0;
+}
+
+void add(int& s, int n){
+    s |= (1 << n);
+}
+
+void remove(int& s, int n){
+    s &= ~(1 << n);
+}
+
+int check(int& s, int n) {
+    if (s & (1 << n)) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+void toggle(int& s, int n) {
+    s ^= (1 << n);
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int m, x, value = 0;
+    int m, x, s = 0;
     string op;
     cin >> m;
     while (m--) {
         cin >> op;
 
-	// x 피연산자가 입력되지 않는 연산자: all, empty
+        // x 피연산자가 입력되지 않는 연산자: all, empty
         if (op == "all") {
-            value = (1 << 21) - 1;
+            all(s);
         }
         else if (op == "empty") {
-            value = 0;
+            empty(s);
         }
 
-	// x 피연산자가 입력되는 연산자: add, remove, check, toggle
+		// x 피연산자가 입력되는 연산자: add, remove, check, toggle
         else {
             cin >> x;
             if (op == "add") {
-                value = value | (1 << x);
+                add(s, x);
             }
             else if (op == "remove") {
-                value = value & ~(1 << x);
+                remove(s, x);
             }
             else if (op == "check") {
-                if (value & (1 << x)) {
-                    cout << "1\n";
-                } else {
-                    cout << "0\n";
-                }
+               cout << check(s, x) << "\n";
             }
             else if (op == "toggle") {
-                value = value ^ (1 << x);
+                toggle(s, x);
             }
         }
     }
